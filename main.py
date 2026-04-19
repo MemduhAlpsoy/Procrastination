75ec6e6c069f0d59d8f48c95f73f0ee235e32269
MemduhAlpsoy <240101009@ogrenci.yalova.edu.tr>
2026-04-19 06:28:46 +0200

    ufak bir branch hatası

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from listener import start_listener
+from enforcer import freeze_process, resume_process, minimize_window
+import time
 
-start_listener()
+def handle_process(captured_process):
+    pid = captured_process[1]
+    freeze_process(pid)
+    print("5 saniyelik dondurma")
+    resume_process(pid)
+
+def handle_window(captured_window):
+    minimize_window(captured_window)
+    print("Pencere minimize edildi")
+
+start_listener(handle_process,handle_window)
