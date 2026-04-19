b4e4b05ed61150451b774ac8b2c25fcd6b6d8de9
MemduhAlpsoy <240101009@ogrenci.yalova.edu.tr>
2026-04-19 06:39:35 +0200

    Bugfix
    Programın zorla kapatıldığında askıda kalan uygulamaların tekrar açılması

--- enforcer/__init__.py
+++ enforcer/__init__.py
@@ -1,2 +1,2 @@
-from process_freezer import freeze_process, resume_process
-from window_freezer import minimize_window  
+from .process_freezer import freeze_process, resume_process
+from .window_freezer import minimize_window  
--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from enforcer import freeze_process, resume_process, minimize_window
 import time
 
 def handle_process(captured_process):
-    pid = captured_process[1]
-    freeze_process(pid)
-    print("5 saniyelik dondurma")
-    resume_process(pid)
+    try:
+        pid = captured_process[1]
+        freeze_process(pid)
+        print("5 saniyelik dondurma")
+        time.sleep(5)
+        resume_process(pid)
+    finally:
+        resume_process(pid)
 
 def handle_window(captured_window):
     minimize_window(captured_window)
